8ec95cf9e145d6792fa2b776115fd72814fdf3fd
NanezX <victor.nanezj@gmail.com>
2021-12-30 17:15:16 -0400

    using nix

--- shell.nix
+++ shell.nix
@@ -7,8 +7,12 @@ let
   '';
 
    all-graph-test = pkgs.writeShellScriptBin "all-graph-test" ''
-    yarn deploy-local;
-    npx hardhat test;
+      npx hardhat node &
+      sleep 30s
+      docker-compose -f "docker/docker-compose.yml" up &
+      sleep 60s
+      npx hardhat test
+      yarn deploy-build:localhost
   '';
   
 in
