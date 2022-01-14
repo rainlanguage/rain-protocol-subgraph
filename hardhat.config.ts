ec3edbe328ba4a2d83be3bcdaa0c2c9f7200885f
NanezX <victor.nanezj@gmail.com>
2022-01-14 10:35:16 -0400

    hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,46 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+function createLocalHostConfig() {
+  const url: string = "http://localhost:8545";
+  const mnemonic: string = "test test test test test test test test test test test junk";
+  return {
+    accounts: {
+      count: 10,
+      initialIndex: 0,
+      mnemonic,
+      path: "m/44'/60'/0'/0",
+    },
+    url,
+  };
+};
+
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "localhost",
+  solidity: "0.8.10",
+  networks: {
+    localhost: createLocalHostConfig(),
+  },
+  mocha: {
+    timeout: 240000,
+  },
+};
+
+export default config;
