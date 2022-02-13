af8a1e797cc8aa0ca68f049e1f75d3d8d75f0277
vishal <vishalkale151071@gmail.com>
2022-02-13 23:22:04 +0530

    added Boilerplate fro escrow.

---
+++ src/redeemableERC20ClaimEscrow/RedeemableERC20ClaimEscrow.ts
@@ -0,0 +1,14 @@
+import { Deposit, PendingDeposit, Undeposit, Withdraw} from "../../generated/RedeemableERC20ClaimEscrow/RedeemableERC20ClaimEscrow"
+
+export function handleDeposit(event: Deposit): void {
+    
+}
+export function handlePendingDeposit(event: PendingDeposit): void {
+
+}
+export function handleUndeposit(event: Undeposit): void {
+
+}
+export function handleWithdraw(event: Withdraw): void {
+
+}
