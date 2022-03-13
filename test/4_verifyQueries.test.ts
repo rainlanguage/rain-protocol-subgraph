5309d6ecfe3cc1f2942ea8b5edb705151523788a
NanezX <victor.nanezj@gmail.com>
2022-03-12 21:11:54 -0400

    finish test

--- test/4_verifyQueries.test.ts
+++ test/4_verifyQueries.test.ts
@@ -1,4 +1,4 @@
-import { expect } from "chai";
+import { expect, assert } from "chai";
 import { hexlify } from "ethers/lib/utils";
 
 import * as Util from "./utils/utils";
@@ -30,11 +30,14 @@ import {
   deployer,
   signer1,
   signer2,
+  seeder1 as signer3,
+  seeder2 as signer4,
   admin,
   // Contracts factory
   verifyFactory,
   noticeBoard,
 } from "./1_trustQueries.test";
+import { ethers } from "hardhat";
 
 let verify: Verify, transaction: ContractTransaction; // use to save/facilite a tx
 
@@ -1224,6 +1227,365 @@ describe("Verify Factory - Queries", function () {
 
       expect(data.requestStatus).to.equals(expectedVerifyAddr.requestStatus);
       expect(data.status).to.equals(expectedVerifyAddr.status);
+    });
+  });
+
+  describe("Verify contract - Verification processs in groups", function () {
+    let eventCounter = 0;
+
+    before(async function () {
+      verify = await Util.verifyDeploy(verifyFactory, deployer, admin.address);
+
+      // Admin grants all roles to himself. This is for testing purposes only, it SHOULD be avoided.
+      await verify.connect(admin).grantRole(APPROVER, admin.address);
+      await verify.connect(admin).grantRole(REMOVER, admin.address);
+      await verify.connect(admin).grantRole(BANNER, admin.address);
+
+      // Adding the admin to be an approved user
+      const approveAdmin = {
+        account: admin.address,
+        data: evidenceAdd,
+      };
+      await verify.connect(admin).approve([approveAdmin]);
+
+      // Increase the event counter since this is add to SubGraph
+      eventCounter++;
+
+      await waitForSubgraphToBeSynced();
+    });
+
+    it("should query all the RequestApproves events when sent them in one single transaction", async function () {
+      const requestApproveAccount1 = {
+        account: signer3.address,
+        data: evidenceApprove,
+      };
+      const counter1 = eventCounter + 1;
+
+      const requestApproveAccount2 = {
+        account: signer4.address,
+        data: evidenceApprove,
+      };
+      const counter2 = eventCounter + 2;
+
+      // RequestApprove in one single transaction
+      transaction = await verify
+        .connect(admin)
+        .request(RequestType.APPROVE, [
+          requestApproveAccount1,
+          requestApproveAccount2,
+        ]);
+
+      // Increase the events counter
+      eventCounter += 2;
+
+      // Wait for sync
+      await waitForSubgraphToBeSynced();
+
+      const verifyRequestApprove1ID = `${verify.address.toLowerCase()} - ${transaction.hash.toLowerCase()} - ${counter1}`;
+      const verifyRequestApprove2ID = `${verify.address.toLowerCase()} - ${transaction.hash.toLowerCase()} - ${counter2}`;
+
+      const query = `
+        {
+          verifyEvents {
+            id
+          }
+          verifyRequestApproves {
+            id
+          }
+        }
+      `;
+      const response = (await subgraph({
+        query,
+      })) as FetchResult;
+      const data = response.data;
+
+      // verifyRequestApprove1
+      expect(data.verifyEvents).to.deep.include({
+        id: verifyRequestApprove1ID,
+      });
+      expect(data.verifyRequestApproves).to.deep.include({
+        id: verifyRequestApprove1ID,
+      });
+
+      // verifyRequestApprove2
+      expect(data.verifyEvents).to.deep.include({
+        id: verifyRequestApprove2ID,
+      });
+      expect(data.verifyRequestApproves).to.deep.include({
+        id: verifyRequestApprove2ID,
+      });
+    });
+
+    it("should query all the RequestRemoves events when sent them in one single transaction", async function () {
+      const requestRemoveAccount1 = {
+        account: signer3.address,
+        data: evidenceRemove,
+      };
+      const counter1 = eventCounter + 1;
+
+      const requestRemoveAccount2 = {
+        account: signer4.address,
+        data: evidenceRemove,
+      };
+      const counter2 = eventCounter + 2;
+
+      // RequestApprove in one single transaction
+      transaction = await verify
+        .connect(admin)
+        .request(RequestType.REMOVE, [
+          requestRemoveAccount1,
+          requestRemoveAccount2,
+        ]);
+
+      // Increase the events counter
+      eventCounter += 2;
+
+      // Wait for sync
+      await waitForSubgraphToBeSynced();
+
+      const verifyRequestRemove1ID = `${verify.address.toLowerCase()} - ${transaction.hash.toLowerCase()} - ${counter1}`;
+      const verifyRequestRemove2ID = `${verify.address.toLowerCase()} - ${transaction.hash.toLowerCase()} - ${counter2}`;
+
+      const query = `
+        {
+          verifyEvents {
+            id
+          }
+          verifyRequestRemoves {
+            id
+          }
+        }
+      `;
+      const response = (await subgraph({
+        query,
+      })) as FetchResult;
+      const data = response.data;
+
+      // verifyRequestRemove1
+      expect(data.verifyEvents).to.deep.include({
+        id: verifyRequestRemove1ID,
+      });
+      expect(data.verifyRequestRemoves).to.deep.include({
+        id: verifyRequestRemove1ID,
+      });
+
+      // verifyRequestRemove2
+      expect(data.verifyEvents).to.deep.include({
+        id: verifyRequestRemove2ID,
+      });
+      expect(data.verifyRequestRemoves).to.deep.include({
+        id: verifyRequestRemove2ID,
+      });
+    });
+
+    it("should query all the RequestBans events when sent them in one single transaction", async function () {
+      const requestBanAccount1 = {
+        account: signer3.address,
+        data: evidenceBan,
+      };
+      const counter1 = eventCounter + 1;
+
+      const requestBanAccount2 = {
+        account: signer4.address,
+        data: evidenceBan,
+      };
+      const counter2 = eventCounter + 2;
+
+      // RequestApprove in one single transaction
+      transaction = await verify
+        .connect(admin)
+        .request(RequestType.BAN, [requestBanAccount1, requestBanAccount2]);
+
+      // Increase the events counter
+      eventCounter += 2;
+
+      // Wait for sync
+      await waitForSubgraphToBeSynced();
+
+      const verifyRequestBan1ID = `${verify.address.toLowerCase()} - ${transaction.hash.toLowerCase()} - ${counter1}`;
+      const verifyRequestBan2ID = `${verify.address.toLowerCase()} - ${transaction.hash.toLowerCase()} - ${counter2}`;
+
+      const query = `
+        {
+          verifyEvents {
+            id
+          }
+          verifyRequestBans {
+            id
+          }
+        }
+      `;
+      const response = (await subgraph({
+        query,
+      })) as FetchResult;
+      const data = response.data;
+
+      // verifyRequestBan1
+      expect(data.verifyEvents).to.deep.include({
+        id: verifyRequestBan1ID,
+      });
+      expect(data.verifyRequestBans).to.deep.include({
+        id: verifyRequestBan1ID,
+      });
+
+      // verifyRequestBan2
+      expect(data.verifyEvents).to.deep.include({
+        id: verifyRequestBan2ID,
+      });
+      expect(data.verifyRequestBans).to.deep.include({
+        id: verifyRequestBan2ID,
+      });
+    });
+
+    it("should query all the approves events when sent them in one single transaction", async function () {
+      const approveAccount1 = {
+        account: signer3.address,
+        data: evidenceAdd,
+      };
+      const counter1 = eventCounter + 1;
+
+      const approveAccount2 = {
+        account: signer4.address,
+        data: evidenceAdd,
+      };
+      const counter2 = eventCounter + 2;
+
+      // Approving in one single transaction
+      transaction = await verify
+        .connect(admin)
+        .approve([approveAccount1, approveAccount2]);
+
+      // Increase the events counter
+      eventCounter += 2;
+
+      // Wait for sync
+      await waitForSubgraphToBeSynced();
+
+      const verifyApprove1ID = `${verify.address.toLowerCase()} - ${transaction.hash.toLowerCase()} - ${counter1}`;
+      const verifyApprove2ID = `${verify.address.toLowerCase()} - ${transaction.hash.toLowerCase()} - ${counter2}`;
+
+      const query = `
+        {
+          verifyEvents {
+            id
+          }
+          verifyApproves {
+            id
+          }
+        }
+      `;
+      const response = (await subgraph({
+        query,
+      })) as FetchResult;
+      const data = response.data;
+
+      // VerifyApprove1
+      expect(data.verifyEvents).to.deep.include({ id: verifyApprove1ID });
+      expect(data.verifyApproves).to.deep.include({ id: verifyApprove1ID });
+
+      // VerifyApprove2
+      expect(data.verifyEvents).to.deep.include({ id: verifyApprove2ID });
+      expect(data.verifyApproves).to.deep.include({ id: verifyApprove2ID });
+    });
+
+    it("should query all the removes events when sent them in one single transaction", async function () {
+      const removeAccount1 = {
+        account: signer3.address,
+        data: evidenceRemove,
+      };
+      const counter1 = eventCounter + 1;
+
+      const removeAccount2 = {
+        account: signer4.address,
+        data: evidenceRemove,
+      };
+      const counter2 = eventCounter + 2;
+
+      // Removing in one single transaction
+      transaction = await verify
+        .connect(admin)
+        .remove([removeAccount1, removeAccount2]);
+
+      // Increase the events counter
+      eventCounter += 2;
+
+      // Wait for sync
+      await waitForSubgraphToBeSynced();
+
+      const verifyRemove1ID = `${verify.address.toLowerCase()} - ${transaction.hash.toLowerCase()} - ${counter1}`;
+      const verifyRemove2ID = `${verify.address.toLowerCase()} - ${transaction.hash.toLowerCase()} - ${counter2}`;
+
+      const query = `
+        {
+          verifyEvents {
+            id
+          }
+          verifyRemoves {
+            id
+          }
+        }
+      `;
+      const response = (await subgraph({
+        query,
+      })) as FetchResult;
+      const data = response.data;
+
+      // VerifyRemove1
+      expect(data.verifyEvents).deep.include({ id: verifyRemove1ID });
+      expect(data.verifyRemoves).deep.include({ id: verifyRemove1ID });
+
+      // VerifyRemove2
+      expect(data.verifyEvents).deep.include({ id: verifyRemove2ID });
+      expect(data.verifyRemoves).deep.include({ id: verifyRemove2ID });
+    });
+
+    it("should query all the ban events when sent them in one single transaction", async function () {
+      const banAccount1 = {
+        account: signer3.address,
+        data: evidenceBan,
+      };
+      const counter1 = eventCounter + 1;
+
+      const banAccount2 = {
+        account: signer4.address,
+        data: evidenceBan,
+      };
+      const counter2 = eventCounter + 2;
+
+      // Banning in one single transaction
+      transaction = await verify.connect(admin).ban([banAccount1, banAccount2]);
+
+      // Increase the events counter
+      eventCounter += 2;
+
+      // Wait for sync
+      await waitForSubgraphToBeSynced();
+
+      const verifyBan1ID = `${verify.address.toLowerCase()} - ${transaction.hash.toLowerCase()} - ${counter1}`;
+      const verifyBan2ID = `${verify.address.toLowerCase()} - ${transaction.hash.toLowerCase()} - ${counter2}`;
+
+      const query = `
+        {
+          verifyEvents {
+            id
+          }
+          verifyBans {
+            id
+          }
+        }
+      `;
+      const response = (await subgraph({
+        query,
+      })) as FetchResult;
+      const data = response.data;
+
+      // VerifyBan1
+      expect(data.verifyEvents).deep.include({ id: verifyBan1ID });
+      expect(data.verifyBans).deep.include({ id: verifyBan1ID });
+
+      // VerifyBan2
+      expect(data.verifyEvents).deep.include({ id: verifyBan2ID });
+      expect(data.verifyBans).deep.include({ id: verifyBan2ID });
     });
   });
 
